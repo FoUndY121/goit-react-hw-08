c0c8f5d814e2d5e795bb55610d668b5334d5765b
FounDy <vkadgcundgd@gmail.com>
2025-04-28 13:14:57 +0200

    ref

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from "./contacts/contactsSlice";
-import filtersReducer from "./filters/filtersSlice";
-import authReducer from "./auth/authSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // localStorage для веб
+import contactsReducer from './contacts/contactsSlice';
+import filtersReducer from './filters/filtersSlice';
+import authReducer from './auth/authSlice';
+
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'], // ми хочемо зберігати лише токен
+};
 
 export const store = configureStore({
     reducer: {
+        auth: persistReducer(authPersistConfig, authReducer),
         contacts: contactsReducer,
         filters: filtersReducer,
-        auth: authReducer,
     },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+export const persistor = persistStore(store);
